66df9398c75334353ae80670b389f74ac4d87345
Caitlyn Barrett <barre786@umn.edu>
2022-09-18 15:08:45 -0500

    update

--- anes.R
+++ anes.R
@@ -1,4 +1,4 @@
-###ANES
+###ANES 2020
 
 #import csv
 anes20<-read.csv("anes_timeseries_2020_csv_20220210.csv")
@@ -21,4 +21,34 @@ library(ggplot2)
 library(tidyverse)
 library(interactions)
 
-#test
+
+############making authoritarianism scale 
+##anes20$V202266 is independence vs respect for elders
+##anes20$V202267 is curiosity vs good manners
+##anes20$V202268 is obedience vs. self-reliance
+##anes20$V202269 is consideration vs. well-behaved
+
+#making all values that are not 1 or 2 NA
+anes20$V202266[anes20$V202266<1]<-NA
+anes20$V202267[anes20$V202267<1]<-NA
+anes20$V202268[anes20$V202268<1]<-NA
+anes20$V202269[anes20$V202269<1]<-NA
+
+anes20$V202266[anes20$V202266==3]<-NA
+anes20$V202267[anes20$V202267==3]<-NA
+anes20$V202268[anes20$V202268==3]<-NA
+anes20$V202269[anes20$V202269==3]<-NA
+
+#0-1 function
+std01<-function(x){
+  min.x<-min(x, na.rm=T)
+  max.x<-max(x-min.x, na.rm=T)
+  return((x-min.x)/max.x)
+}
+##0-1 the four auth variables
+anes20$V202266<-std01(anes20$V202266)
+anes20$V202267<-std01(anes20$V202267)
+anes20$V202268<-std01(anes20$V202268)
+anes20$V202269<-std01(anes20$V202269)
+
+
