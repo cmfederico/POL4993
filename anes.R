6f2a7a0bac36211f3c9a13a40a278a8063998739
Caitlyn Barrett <barre786@umn.edu>
2022-09-18 14:36:31 -0500

    Update anes.R

--- anes.R
+++ anes.R
@@ -1,4 +1,23 @@
 ###ANES
 
 #import csv
-anes20<-read.csv("C:/Users/Caity/POL4993/anes_timeseries_2020_csv_20220210.csv")
+anes20<-read.csv("anes_timeseries_2020_csv_20220210.csv")
+
+#load libraries
+library(car)
+library(psych)
+library(ggplot2)
+library(AER)
+library(estimatr)
+library(summarytools)
+library(dotwhisker)
+library(dplyr)
+library(lavaan)
+library(semPlot)
+library(semTools)
+library(hrbrthemes)
+library(survey)
+library(ggplot2)
+library(tidyverse)
+library(interactions)
+
